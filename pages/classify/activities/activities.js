f8a33b6068b907cf51963220325e72744d4409cb
lunchunhua <1757661652@qq.com>
2018-12-19 17:41:27 +0800

    发现页面

--- pages/classify/activities/activities.js
+++ pages/classify/activities/activities.js
@@ -1,3 +1,73 @@
 const App = getApp();
-const util = require('../../utils/util.js');
-const api = require('../../utils/api.js');
+const util = require('../../../utils/util.js');
+const api = require('../../../utils/api.js');
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   
+  },
+
+  //跳转到详情页
+  // productlist: function (e) {
+  //   wx.navigateTo({
+  //     url: '/pages/classify/particulars/particulars',
+  //   })
+  // },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+ * 生命周期函数--监听页面初次渲染完成
+ */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
